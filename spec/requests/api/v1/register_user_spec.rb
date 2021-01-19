c92f71091acd7cbca4d117894a34eb8ad80984b9
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 00:13:31 -0700

    Add test for users endpoint

--- spec/requests/api/v1/register_user_spec.rb
+++ spec/requests/api/v1/register_user_spec.rb
@@ -5,24 +5,23 @@ RSpec.describe "User endpoints" do
     it "can return user info upon registration" do
 
       query_params = {
-          "email": "whatever@example.com",
+          "email": "whatever7@example.com",
           "password": "password",
           "password_confirmation": "password"
-                    }
-      get "/api/v1/users", params: query_params
-      
+                      }
+      post "/api/v1/users", params: query_params
       expect(response).to be_successful
       expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
-
+      
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response).to be_a(Hash)
       expect(user_response).to have_key(:data)
       expect(user_response[:data]).to be_a(Hash)
       expect(user_response[:data]).to have_key(:id)
-      expect(user_response[:data][:id]).to eq(nil)
+      expect(user_response[:data][:id]).to be_a(String)
       expect(user_response[:data]).to have_key(:type)
-      expect(user_response[:data][:type]).to eq('image')
+      expect(user_response[:data][:type]).to eq('users')
       expect(user_response[:data]).to have_key(:attributes)
       expect(user_response[:data][:attributes]).to be_a(Hash)
 
