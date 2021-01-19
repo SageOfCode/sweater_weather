3b099fce09ae49a0a32522b4509bc66d4fb77e57
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 09:50:56 -0700

    Add distance call to index action

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
+    conn = Faraday.new("http://www.mapquestapi.com") do |f|
+      f.params['key'] = ENV['map_api_key']
+    end
+    response = conn.get("/directions/v2/route") do |r|
+      r.params['from'] = params[:start]
+      r.params['to'] = params[:destination]
+    end 
+    json = JSON.parse(response.body, symbolize_names: true)
+
     require 'pry'; binding.pry
   end
 end 
