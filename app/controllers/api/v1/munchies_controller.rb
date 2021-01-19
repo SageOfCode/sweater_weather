43220907784ebe92c7103ad79e6aee7e85279cd2
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 11:23:05 -0700

    Move facade calls from controller to facade

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,13 +1,6 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    distance_data = LocationFacade.get_distance(params[:start], params[:destination])
-
-    forecast_data = WeatherFacade.get_food_weather(distance_data[:route][:boundingBox][:lr][:lat], distance_data[:route][:boundingBox][:lr][:lng])
-    
-    restaurant_data = RestaurantFacade.restaurant_data(params[:food], params[:destination])
-
-    munchie_object = Munchie.new(params[:destination], distance_data, forecast_data, restaurant_data)
-    
+    munchie_object = RestaurantFacade.create_munchie_obj(params[:start], params[:destination], params[:food])
     render json: MunchieSerializer.new(munchie_object)
   end
 end 
--- app/facades/restaurant_facade.rb
+++ app/facades/restaurant_facade.rb
@@ -2,4 +2,12 @@ class RestaurantFacade
   def self.restaurant_data(food_type, location)
     YelpService.get_food_data(food_type, location)
   end
+
+  def self.create_munchie_obj(start, destination, food)
+    distance_data = LocationFacade.get_distance(start, destination)
+    forecast_data = WeatherFacade.get_food_weather(distance_data[:route][:boundingBox][:lr][:lat], distance_data[:route][:boundingBox][:lr][:lng])
+    restaurant_data = restaurant_data(food, destination)
+
+    Munchie.new(destination, distance_data, forecast_data, restaurant_data)
+  end
 end
