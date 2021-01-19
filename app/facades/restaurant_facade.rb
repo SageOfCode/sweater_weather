92dff14cfd06a004918aa4d12c98e3f248afc7e7
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 11:56:36 -0700

    Attempted time add feature, did not complete

--- app/facades/restaurant_facade.rb
+++ app/facades/restaurant_facade.rb
@@ -7,14 +7,14 @@ class RestaurantFacade
     def create_munchie_obj(start, destination, food)
       distance_data = LocationFacade.get_distance(start, destination)
       forecast_data = WeatherFacade.get_food_weather(distance_data[:route][:boundingBox][:lr][:lat], distance_data[:route][:boundingBox][:lr][:lng])
-      arrival_time = arrive_time(distance_data)
+      # arrival_time = arrive_time(distance_data)
       restaurant_data = restaurant_data(food, destination, arrival_time)
 
       Munchie.new(destination, distance_data, forecast_data, restaurant_data)
     end
 
     def arrive_time(distance_data)
-      require 'pry'; binding.pry
+      Time.parse(distance_data[:route][:formattedTime]).min + Time.now
     end
   end 
 end
