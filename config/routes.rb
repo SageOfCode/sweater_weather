7927e806c9403102c62b3d520ce06c1646760b2e
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 00:14:16 -0700

    Add route for user controller create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
