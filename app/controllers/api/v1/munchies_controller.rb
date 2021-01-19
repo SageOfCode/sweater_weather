6b59d43066883aec5834cba7ef4c9960e2aac001
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 10:04:39 -0700

    Add additional api call for forecast and food

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -7,7 +7,25 @@ class Api::V1::MunchiesController < ApplicationController
       r.params['from'] = params[:start]
       r.params['to'] = params[:destination]
     end 
-    json = JSON.parse(response.body, symbolize_names: true)
+    distance_data = JSON.parse(response.body, symbolize_names: true)
+    
+    conn2 = Faraday.new(url: 'http://api.openweathermap.org') do |req|
+      req.params['appid'] = ENV['weather_api_key']
+    end
+    response2 = conn2.get('/data/2.5/onecall') do |req|
+      req.params['lat'] = distance_data[:route][:boundingBox][:lr][:lat]
+      req.params['lon'] = distance_data[:route][:boundingBox][:lr][:lng]
+    end
+    forecast_data = JSON.parse(response2.body, symbolize_names: true)
+ 
+    conn3 = Faraday.new(url: 'https://api.yelp.com') do |req|
+      req.headers["Authorization"] = ENV['yelp_api_key']
+    end
+    response3 = conn3.get('/v3/businesses/search') do |req|
+      req.params['term'] = params[:food]
+      req.params['location'] = params[:destination]
+    end
+    restaurant_data = JSON.parse(response3.body, symbolize_names: true)
 
     require 'pry'; binding.pry
   end
