d12c6cf349c0f027fd0498ce529c4030b9b51cad
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 10:31:08 -0700

    Complete munchie poro

--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -1,7 +1,19 @@
 class Munchie
+  attr_reader :destination_city,
+              :travel_time,
+              :forecast,
+              :restaurant
+
   def initialize(city, distance, forecast, food)
-    require 'pry'; binding.pry
     @destination_city = city.gsub(",", ", ").capitalize
     @travel_time = distance[:route][:formattedTime]
+    @forecast = {
+      summary: forecast[:current][:weather][0][:description],
+      temperature: forecast[:current][:temp].to_s
+    }
+    @restaurant = {
+      name: food[:businesses][0][:name],
+      address: food[:businesses][0][:location][:display_address].join(" ")
+    }
   end
 end
