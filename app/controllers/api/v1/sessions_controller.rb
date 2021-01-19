61a03cc8d17e7d0f9c70614527491c5024f6c9fc
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 00:51:43 -0700

    Create sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    # require 'pry'; binding.pry
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UsersSerializer.new(user)
+    else
+      render json: "Invalid Credentials", status: 401
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+end
