6443ee77fec7c956d70e39d2e262e4848a880ed8
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-20 00:21:11 -0700

    Fix conditional logic

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    image = ImageFacade.grab_image(params[:location])
-    if image 
+    if params[:location] != ""
+      image = ImageFacade.grab_image(params[:location])
       render json: ImageSerializer.new(image)
     else 
       render body: "Invalid Query", status: 400
