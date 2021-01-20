d56f4cc0000c52a9b9aa2a60466bcbe7e8c51a81
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 19:18:35 -0700

    Add sad path testing

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    # require 'pry'; binding.pry
     user = User.find_by(api_key: params[:api_key])
     if user
       road_trip = RoadtripFacade.get_road_trip_data(params[:origin], params[:destination])
