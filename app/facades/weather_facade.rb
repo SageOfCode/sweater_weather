f10fb51e53bd5441e09d79791c798edb09945d32
Sage Gonzales <sagegonzales15@gmail.com>
2021-01-19 22:38:44 -0700

    Refactor to one method in service

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -6,7 +6,7 @@ class WeatherFacade
     end
     
     def get_current_weather(coords)
-      data = WeatherService.current_weather(coords)
+      data = WeatherService.weather_forecast(coords.lat, coords.lng)
       current_weather = CurrentWeather.new(data[:current])
       daily_weather = data[:daily][0..4].map { |json| DailyWeather.new(json) }
       hourly_weather = data[:hourly][0..7].map { |json| HourlyWeather.new(json) }
